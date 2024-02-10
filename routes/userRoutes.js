dcf38d4a1760f1a46b680e912d28057a01a48041
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-02-11 01:01:52 +0600

    working on search user feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {registerUser, authUser} = require("../controllers/userControllers")
 const router = express.Router();
 
-router.route("/").post(registerUser)
+router.route("/").post(registerUser).get(allUsers);
 router.post("/login", authUser)
 
 module.exports = router;
