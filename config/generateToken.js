2f86e0c9b464f98f41fb9322a8d9692aa1c41b56
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-02-12 00:08:12 +0600

    working on the chatController

--- config/generateToken.js
+++ config/generateToken.js
@@ -1,9 +1,9 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const generateToken = (id)=>{
-    return jwt.sign({id}, process.env.JWT_SECRET_KEY,{
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
         expiresIn: "30d",
-    })
-}   
+    });
+};
 
 module.exports = generateToken;
